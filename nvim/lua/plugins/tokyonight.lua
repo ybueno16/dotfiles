95cc1a1c3ce1703cdb93c3b553ca88f8939e1a52
ybueno16 <yuribueno27@gmail.com>
2024-07-30 16:35:04 -0300

    feat: alterar colorscheme de tokyonigth para catpuccin

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -1,11 +1,31 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-	config = function()
-		vim.cmd.colorscheme "tokyonight-moon"
-	end
-    }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd("colorscheme catppuccin-mocha")
+			require("catppuccin").setup({
+				styles = {
+					comments = { "italic" }, -- Change the style of comments
+				},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					treesitter = true,
+					barbecue = {
+						dim_dirname = true, -- directory name is dimmed by default
+						bold_basename = true,
+						dim_context = false,
+						alt_background = false,
+					},
+					telescope = {
+						enabled = true,
+					},
+				},
+			})
+		end,
+	},
 }
