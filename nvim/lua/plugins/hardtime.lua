871c265e44f4d6bba064a735041e8bde02c520a0
vr-yuribueno <yuri.bueno@vrsoft.com.br>
2025-02-20 09:50:29 -0300

    feat:adicionar autosave hardtime rainbow-brackets e treesitter-context

---
+++ nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,10 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	opts = {
+		enabled = true,
+		showmode = false,
+		cmdheight = 2,
+          disable_mouse = false,
+	},
+}
